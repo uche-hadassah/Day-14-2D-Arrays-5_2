8d94326759de1ccd2928e197a9024275a9984e36
Hadassah Uche <uhadassah1@gmail.com>
2023-11-14 18:42:22 +0200

    i used in the second loop to set the diagonals. Turns out i was trying to subtract  and i from 0. All i had to do was use size. I didnt need the 2nd loop either

--- UcheHadassah5_2/HadassahUche5_2.cpp
+++ UcheHadassah5_2/HadassahUche5_2.cpp
@@ -1,5 +1,5 @@
-/*Write a program that creates and prints a two-dimensional square array that looks 
-as the following examples:
+/*Name: Uche Hadassah
+This program prints a 2-D array that looks like:
 *--------* 
 -*------*- 
 --*----*-- 
@@ -9,8 +9,9 @@ as the following examples:
 ---*--*--- -*-*-
 --*----*-- --*--
 -*------*- -*-*-
-*--------* *---*
-for 10 rows and 10 columns for 5 rows and 5 columns*/
+*--------* *---*   
+[10][10]   [5][5]
+ */
 #include<iostream>
 using namespace std;
 int main()
@@ -18,17 +19,18 @@ int main()
 	const int SIZE = 100;
 	int size;
 	char squareArray[SIZE][SIZE];
+	//Receiving the dimension of the square array from the user
 	cout << "Enter the size of the square array:";
 	do
 	{
 		cin >> size;
-		while (size < 1)
+		while (size < 1 || size > SIZE)
 		{
-			cout << "Invalid! Enter size: ";
+			cout << "Invalid! Enter a valid size(from 1 to 100 included): ";
 			cin >> size;
 		}
-	} while (size < 1);
-	//Initialize all the elements in the array to -
+	} while (size < 1 || size > SIZE);//Verifying user input
+	//Initialize all the elements in the array to '-'
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
@@ -36,11 +38,11 @@ int main()
 			squareArray[i][j] = '-';
 		}
 	}
-	//Set all the diagonals to *
+	//Set all the diagonals to '*'
 	for (int i = 0; i < size; i++)
 	{
-			squareArray[i][i] = '*';
-			squareArray[i][size - 1 - i] = '*';
+			squareArray[i][i] = '*';//All situations where we have [i][i] will be set to '*'
+			squareArray[i][size - 1 - i] = '*';//Same for this
 	}
 	//Output the square array
 	for (int i = 0; i < size; i++)
